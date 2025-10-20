a8f289ecb271ae0687a31f511a75f5bb38dbfd6d
shirakami <git.rwoov@simplelogin.com>
2025-10-21 00:13:06 +0900

    add packages.

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,9 +2,10 @@
 
 set -ouex pipefail
 
-### Add nix
+### add mount target /nix
 
-# mkdir -p /nix
+# lix ostree installer use /home/nix, and mount on /nix
+mkdir -p /nix
 
 ### Install packages
 
@@ -13,15 +14,19 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs a package from fedora repos
-# dnf5 install -y tmux
+dnf5 install fcitx5 fcitx5-gtk fcitx5-mozc
 
-# Use a COPR Example:
+# Use a COPR
 #
 dnf5 -y copr enable yalter/niri
 dnf5 -y install niri
+
+dnf5 -y copr enable blakegardner/xremap
+dnf5 install xremap-niri
+
 # Disable COPRs so they don't end up enabled on the final image:
 dnf5 -y copr disable yalter/niri
+dnf5 -y copr disable blakegardner/xremap
 
 #### Example for enabling a System Unit File
 
