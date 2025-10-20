4be5e787f1aa6a0aad1df0cba8910d3ad0bcb438
shirakami <git.rwoov@simplelogin.com>
2025-10-20 19:28:02 +0900

    init.

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,6 +2,10 @@
 
 set -ouex pipefail
 
+### Add nix
+
+mkdir -p /nix
+
 ### Install packages
 
 # Packages can be installed from any enabled yum repo on the image.
@@ -10,14 +14,14 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+# dnf5 install -y tmux
 
 # Use a COPR Example:
 #
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
+dnf5 -y copr enable yalter/niri
+dnf5 -y install niri
 # Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
+dnf5 -y copr disable yalter/niri
 
 #### Example for enabling a System Unit File
 
